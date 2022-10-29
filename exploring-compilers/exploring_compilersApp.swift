2f83ee3fa0827f2df60b1a00b831e4dfd0f65aa8
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-10-30 00:32:14 +0200

    Initial Commit

---
+++ exploring-compilers/exploring_compilersApp.swift
@@ -0,0 +1,17 @@
+//
+//  exploring_compilersApp.swift
+//  exploring-compilers
+//
+//  Created by Javier on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct exploring_compilersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ exploring-compilersUITests/exploring_compilersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  exploring_compilersUITestsLaunchTests.swift
+//  exploring-compilersUITests
+//
+//  Created by Javier on 30.10.22.
+//
+
+import XCTest
+
+final class exploring_compilersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
